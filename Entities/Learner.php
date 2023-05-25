198496a6729a6b14722a77b1b873f040e339ff9f
Dedan Irungu <dedanirungu@gmail.com>
2023-05-25 10:31:29 +0300

    Adding New Modules

---
+++ Entities/Learner.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Tertiary\Entities;
+
+use Modules\Base\Entities\BaseModel;
+use Illuminate\Database\Schema\Blueprint;
+
+class Student extends BaseModel
+{
+
+    protected $fillable = ['name'];
+    public $migrationDependancy = [];
+    protected $table = "tertiary_student";
+
+    /**
+     * List of fields for managing postings.
+     *
+     * @param Blueprint $table
+     * @return void
+     */
+    public function migration(Blueprint $table)
+    {
+        $table->increments('id');
+        $table->string('name');
+    }
+}
---
+++ right.php
@@ -0,0 +1,8 @@
+<?php
+
+$this->add_right("tertiary", "tertiary", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "tertiary", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "tertiary", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "tertiary", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "tertiary", "registered", view:true, add:true);
+$this->add_right("tertiary", "tertiary", "guest", view:true, );
