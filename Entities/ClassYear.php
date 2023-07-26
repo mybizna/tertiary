0c336507be463122d39bfeb6356373ef4d0930c2
Dedan Irungu <dedanirungu@gmail.com>
2023-07-26 14:00:30 +0300

    adding listtable and formbuilder fuction to all model so that data can be rendered via json

--- Entities/ClassYear.php
+++ Entities/ClassYear.php
@@ -5,6 +5,9 @@ namespace Modules\Tertiary\Entities;
 use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
 
+use Modules\Core\Classes\Views\ListTable;
+use Modules\Core\Classes\Views\FormBuilder;
+
 class ClassYear extends BaseModel
 {
 
@@ -12,6 +15,36 @@ class ClassYear extends BaseModel
     public $migrationDependancy = [];
     protected $table = "tertiary_class_year";
 
+
+    public function listTable(){
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('name')->type('text')->ordering(true);
+
+        return $fields;
+
+    }
+    
+    public function formBuilder(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/2');
+
+        return $fields;
+
+    }
+
+    public function filter(){
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/6');
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
