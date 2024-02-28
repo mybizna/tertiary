6a98b81817d348ea9b6f16cd94e7743773b54032
Dedan <dedanirungu@gmail.com>
2024-02-28 11:22:38 +0300

    Update form layout in Entities classes

--- Entities/ClassYear.php
+++ Entities/ClassYear.php
@@ -61,7 +61,7 @@ class ClassYear extends BaseModel
         $structure['form'] = [
             ['label' => 'Name', 'class' => 'col-span-full', 'fields' => ['name']],
             ['label' => 'Description', 'class' => 'col-span-full', 'fields' => ['description']],
-            ['label' => 'Published', 'class' => 'col-span-full md:col-span-6', 'fields' => ['published']],
+            ['label' => 'Published', 'class' => 'col-span-full  md:col-span-6 md:pr-2', 'fields' => ['published']],
         ];
         $structure['filter'] = ['name', 'published'];
 
