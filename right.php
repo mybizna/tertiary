6706a31d51d04dd0379158e57b21a8c6846b10e1
Dedan <dedanirungu@gmail.com>
2024-04-01 19:30:37 +0300

    Add rights for various user roles and entities

--- right.php
+++ right.php
@@ -2,9 +2,74 @@
 
 /** @var \Modules\Base\Classes\Fetch\Rights $this */
 
-$this->add_right("tertiary", "tertiary", "administrator", view:true, add:true, edit:true, delete:true);
-$this->add_right("tertiary", "tertiary", "manager", view:true, add:true, edit:true, delete:true);
-$this->add_right("tertiary", "tertiary", "supervisor", view:true, add:true, edit:true, delete:true);
-$this->add_right("tertiary", "tertiary", "staff", view:true, add:true, edit:true);
-$this->add_right("tertiary", "tertiary", "registered", view:true, add:true);
-$this->add_right("tertiary", "tertiary", "guest", view:true, );
+
+$this->add_right("tertiary", "class_year", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "class_year", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "class_year", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "class_year", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "class_year", "registered", view:true, add:true);
+$this->add_right("tertiary", "class_year", "guest", view:true, );
+
+$this->add_right("tertiary", "department", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "department", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "department", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "department", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "department", "registered", view:true, add:true);
+$this->add_right("tertiary", "department", "guest", view:true, );
+
+$this->add_right("tertiary", "learner_course", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner_course", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner_course", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner_course", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "learner_course", "registered", view:true, add:true);
+$this->add_right("tertiary", "learner_course", "guest", view:true, );
+
+$this->add_right("tertiary", "program", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "program", "manager", view:true, add:true, edit:true, delete:true);    
+$this->add_right("tertiary", "program", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "program", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "program", "registered", view:true, add:true);
+$this->add_right("tertiary", "program", "guest", view:true, );
+
+
+$this->add_right("tertiary", "course", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "course", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "course", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "course", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "course", "registered", view:true, add:true);
+$this->add_right("tertiary", "course", "guest", view:true, );
+
+$this->add_right("tertiary", "faculty", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "faculty", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "faculty", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "faculty", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "faculty", "registered", view:true, add:true);
+$this->add_right("tertiary", "faculty", "guest", view:true, );
+
+$this->add_right("tertiary", "learner", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "learner", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "learner", "registered", view:true, add:true);
+$this->add_right("tertiary", "learner", "guest", view:true, );
+
+$this->add_right("tertiary", "qualification", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "qualification", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "qualification", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "qualification", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "qualification", "registered", view:true, add:true);
+$this->add_right("tertiary", "qualification", "guest", view:true, );
+
+$this->add_right("tertiary", "school", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "school", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "school", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "school", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "school", "registered", view:true, add:true);
+$this->add_right("tertiary", "school", "guest", view:true, );
+
+$this->add_right("tertiary", "subject", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "subject", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "subject", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("tertiary", "subject", "staff", view:true, add:true, edit:true);
+$this->add_right("tertiary", "subject", "registered", view:true, add:true);
+$this->add_right("tertiary", "subject", "guest", view:true, );
