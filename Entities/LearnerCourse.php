570cedb980282227fa5be4ae4831bb335fe95ee9
Dedan <dedanirungu@gmail.com>
2024-04-12 11:16:06 +0300

    Add rights() method to ClassYear, Course, Department, Faculty, Learner, LearnerCourse, Program, Qualification, School, and Tertiary models

--- Entities/LearnerCourse.php
+++ Entities/LearnerCourse.php
@@ -75,6 +75,12 @@ class LearnerCourse extends BaseModel
      */
     public function rights(): array
     {
+        $rights = parent::rights();
 
+        $rights['staff'] = ['view' => true];
+        $rights['registered'] = ['view' => true];
+        $rights['guest'] = [];
+
+        return $rights;
     }
 }
