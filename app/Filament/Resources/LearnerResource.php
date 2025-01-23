6ad2696a960c1451980ec9013b133100ebbc7c4a
Dedan <dedanirungu@gmail.com>
2025-01-23 03:42:39 +0300

    Make Filament Resource Shorter

--- app/Filament/Resources/LearnerResource.php
+++ app/Filament/Resources/LearnerResource.php
@@ -2,11 +2,7 @@
 
 namespace Modules\Tertiary\Filament\Resources;
 
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Tertiary\Filament\Resources\LearnerResource\Pages;
 use Modules\Tertiary\Models\Learner;
 
 class LearnerResource extends BaseResource
@@ -19,42 +15,4 @@ class LearnerResource extends BaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
--- app/Filament/Resources/TertiaryResource.php
+++ app/Filament/Resources/TertiaryResource.php
@@ -2,11 +2,7 @@
 
 namespace Modules\Tertiary\Filament\Resources;
 
-use Filament\Forms\Form;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
-use Modules\Tertiary\Filament\Resources\TertiaryResource\Pages;
 use Modules\Tertiary\Models\Tertiary;
 
 class TertiaryResource extends BaseResource
@@ -19,42 +15,4 @@ class TertiaryResource extends BaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                //
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                //
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
-    public static function getPages(): array
-    {
-
-        return [
-            'index' => Pages\Listing::route('/'),
-            'create' => Pages\Creating::route('/create'),
-            'edit' => Pages\Editing::route('/{record}/edit'),
-        ];
-    }
 }
